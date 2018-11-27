fd4053350ae863a1a30e10d62f29da41b86200e2
Jan Lindblom <janlindblom@fastmail.fm>
2018-11-27 14:07:58 +0200

    Imported C# code (converted).

--- main.ts
+++ main.ts
@@ -4,6 +4,28 @@ namespace roman {
     //% blockId=roman_show_number
     //% block="show|number %num"
     export function showNumber(num: number, interval: number = 150): void {
+        let arabic: number = num
+        let thouLetters: string[] = ["", "M", "MM", "MMM"]
+        let hundLetters: string[] = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+        let tensLetters: string[] = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+        let onesLetters: string[] = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+
+        let result = ""
+        let numb: number = 0
+        numb = Math.idiv(arabic, 1000)
+        result += thouLetters[numb]
+        arabic %= 1000
+
+        numb = Math.idiv(arabic, 100)
+        result += hundLetters[numb]
+        arabic %= 100
+
+        numb = Math.idiv(arabic, 10)
+        result += tensLetters[numb]
+        arabic %= 10
+
+        result += onesLetters[arabic]
+
         basic.showString(Math.roundWithPrecision(num, 2).toString(), interval)
     }
 }
