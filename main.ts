05e23bc794dbd8010c68fb0cfd59c9a52f46da31
Jan Lindblom <janlindblom@fastmail.fm>
2018-11-27 13:55:54 +0200

    Basics.

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+//% color=190 weight=100 icon="\uf25b" block="Roman Numerals"
+//% groups=['LED matrix', 'others']
+namespace roman {
+    //% blockId=roman_show_number
+    //% block="show|number %num"
+    export function showNumber(num: number, interval: number = 150): void {
+        basic.showString(Math.roundWithPrecision(num, 2).toString(), interval)
+    }
+}
+
 basic.showLeds(`
     . . . . .
     . # . # .
